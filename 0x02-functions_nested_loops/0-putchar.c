cf4a2ecf7341be009d69ad29804538b9b5e4bd74
snum01 <johnjim4life@gmail.com>
2022-09-13 10:17:51 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,15 @@
-#include "holberton.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- * main -  prints Holberton, followed by a new line
- * Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always success
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
 
-	return (0);
+printf("_putchar\n");
+
+return (0);
 }
