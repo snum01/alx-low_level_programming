8672a2cb5f5d93170927eda58dfbd5f1eb103957
snum01 <johnjim4life@gmail.com>
2022-09-13 12:10:13 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,6 @@
  * main - print putchar
  * Return: always 0
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -16,6 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
 	return (0);
 }
