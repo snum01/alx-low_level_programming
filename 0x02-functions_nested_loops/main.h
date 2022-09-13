31936a721d5331aff517abfe99d6e9c86e995c51
snum01 <johnjim4life@gmail.com>
2022-09-13 11:03:42 -0700

    _Putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,19 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
 
 void print_alphabet(void);
-print_alphabet_x10(void);
-int islower(int c);
-int _putchar(char c);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _putchar(char);
 int _isalpha(int c);
-int print_sign(int c);
+int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
+void num(int r, int n);
 
 #endif
