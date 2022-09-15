9806c11483ce735a2c37416db35e10af3cf9a436
snum01 <johnjim4life@gmail.com>
2022-09-14 21:10:56 -0700

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - prints positive or negative integer
  * Return: 0
@@ -7,6 +8,7 @@ int main(void)
 {
 	int i;
 	i = 0;
+
 	positive_or_negative(i);
 	return (0);
 }
